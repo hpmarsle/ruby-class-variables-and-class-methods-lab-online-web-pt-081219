70c3fdf7aea579e604b2b470f21c321ed009d1d9
Mars Le <hp.mars.le@gmail.com>
2019-08-27 02:05:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,13 @@ class Song
   end 
   
   def self.artist_count
-    @@artist_count
+    @@artists.each do |artist| 
+      if @@artist_count.has_key?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end 
+    end 
   end 
   
 end 
