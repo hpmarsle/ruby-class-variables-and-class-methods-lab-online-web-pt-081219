bf9f3fb0e92054674eda92fcd46233ec2b817e91
Mars Le <hp.mars.le@gmail.com>
2019-08-27 02:05:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,7 @@ class Song
         @@genre_count[genre] = 1
       end 
     end 
+    @@genre_count
   end 
   
   def self.artist_count
@@ -62,7 +63,8 @@ class Song
       else
         @@artist_count[artist] = 1
       end 
-    end 
+    end
+    @@artist_count
   end 
   
 end 
